1d1fb4f946051ed8ce78528d60e7b386dc541940
Davi William Moraes Suga <daviciencia1@gmail.com>
2021-08-07 11:37:02 -0300

    added post example

--- app.ts
+++ app.ts
@@ -1,5 +1,6 @@
 import app from "./framework.ts";
 import { FancyReq } from "./types.ts";
+import { pipe } from "./lib/pipe.ts";
 
 const showMethod = (req: FancyReq) => {
   return `Oh, you sent a ${req.method}`;
@@ -7,13 +8,29 @@ const showMethod = (req: FancyReq) => {
 const showParams = (req: FancyReq) => {
   return `Your params: ${JSON.stringify(req.query)}`;
 };
+
+const log = <T>(something: T) => {
+  console.log(something);
+  return something;
+};
+
+const getTodoBody = (req: FancyReq) => req.json();
+const createTodo = async <T extends unknown>(todoBody?: Promise<T>) => {
+  const resolved = await todoBody;
+  console.log(resolved);
+  return ({ created: true })
+};
+
+const postTodo = pipe(getTodoBody, createTodo);
+
 app()
   .onGet({
     "/method": showMethod,
-    "/ssas": () => "SSASS",
+    "/ssas/:id": () => "SSASS",
     "/params": showParams,
   })
   .onPost({
+    "/todos/new": postTodo,
     "/posty": showMethod,
   })
   .init();

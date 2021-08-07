128a08c89688a0b93b4de2359646a7d89c45d56b
Davi William Moraes Suga <daviciencia1@gmail.com>
2021-08-07 11:37:09 -0300

    documented types

--- types.ts
+++ types.ts
@@ -2,12 +2,16 @@ export type CanBeEncoded =
   | string
   | number
   | boolean
+  | void
   | Record<string | number, unknown>;
 
 export type FancyReq = Request & {
   query: Record<string, string>;
 };
 
+/**
+ * Is any function that receives a request and returns serializable stuff.
+ */
 export type RequestHandler = (
   req: FancyReq
 ) => CanBeEncoded | Response | Promise<CanBeEncoded> | Promise<Response>;
@@ -16,15 +20,22 @@ export type Controller = (req: Request) => Response | Promise<Response>;
 
 export type ToController = (reqHandler: RequestHandler) => Controller;
 
+/**
+ * Maps each route it's a controller, a.k.a. "Request Handler".
+ */
 export type ReqMap = {
   [route: string]: RequestHandler;
 };
 
-export type MethodHandler = (reMap: ReqMap) => UnnamedAPI;
-export type HttpMethod = "GET" | "POST" | "PUT" | "DELETE"; //| "HEAD" | "OPTIONS" | "UPDATE";
+/**
+ * Describes the routes of an API from a given method.
+ */
 export type RouteMap = {
   [method in HttpMethod]?: ReqMap;
 };
+
+export type MethodHandler = (reMap: ReqMap) => UnnamedAPI;
+export type HttpMethod = "GET" | "POST" | "PUT" | "DELETE"; //| "HEAD" | "OPTIONS" | "UPDATE";
 
 export type UnnamedAPI = {
   init: (options: Deno.ListenOptions) => void;

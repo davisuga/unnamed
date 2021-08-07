1dcf743fa55fa0b80ba1fcb5d70dd4a1fcae3cf8
Davi William Moraes Suga <daviciencia1@gmail.com>
2021-08-07 12:44:31 -0300

    added redis todo example

--- app.ts
+++ app.ts
@@ -1,6 +1,15 @@
 import app from "./framework.ts";
 import { FancyReq } from "./types.ts";
 import { pipe } from "./lib/pipe.ts";
+import { connect } from "https://denopkg.com/keroxp/deno-redis/mod.ts";
+
+const redis = await connect({
+  hostname: "127.0.0.1",
+  port: 6379,
+});
+
+const ok = await redis.set("hoge", "fuga");
+const fuga = await redis.get("hoge");
 
 const showMethod = (req: FancyReq) => {
   return `Oh, you sent a ${req.method}`;
@@ -9,18 +18,48 @@ const showParams = (req: FancyReq) => {
   return `Your params: ${JSON.stringify(req.query)}`;
 };
 
-const log = <T>(something: T) => {
+const log = <T extends any>(something: any):T => {
   console.log(something);
   return something;
 };
 
-const getTodoBody = (req: FancyReq) => req.json();
-const createTodo = async <T extends unknown>(todoBody?: Promise<T>) => {
-  const resolved = await todoBody;
-  console.log(resolved);
-  return ({ created: true })
+const trace = (label: string | Function) => <T>(value: T):any => {
+  if(typeof label === "function") {
+    label(value)
+    return value
+  }
+  console.log(label, value)
+  return value
+}
+
+type TodoContent = {
+  uid: string;
+  content: string;
 };
 
+const getTodoBody = (req: FancyReq) => req.json();
+
+const createTodo = async (todoBody?: Promise<TodoContent>) => {
+  const resolved = await todoBody;
+  console.log(resolved);
+  if (!resolved?.uid) {
+    return ({ created: false });
+  }
+  const { uid, content } = resolved;
+  const result = await redis.sadd(`todos:${uid}`, content);
+  return ({ created: result });
+};
+
+const readTodos = async (uid: string) => {
+  console.log({uid})
+  const result = await redis.smembers(`todos:${uid}`);
+  console.log({ redisResult: result });
+  return result
+};
+
+const getQueryID = (req: FancyReq) => req.query.uid;
+
+const getTodos = pipe(trace((x: FancyReq) => console.log(x.query)), getQueryID, readTodos);
 const postTodo = pipe(getTodoBody, createTodo);
 
 app()
@@ -28,6 +67,7 @@ app()
     "/method": showMethod,
     "/ssas/:id": () => "SSASS",
     "/params": showParams,
+    "/todos": getTodos,
   })
   .onPost({
     "/todos/new": postTodo,
